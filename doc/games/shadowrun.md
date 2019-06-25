83119477eedf81ada92b773c7c7220d7d355672c
Felix Münscher <Felix.Muenscher@mni.thm.de>
2019-06-25 22:42:25 +0200

    Adds all metatypes and some more data

---
+++ doc/games/shadowrun.md
@@ -0,0 +1,13 @@
+# Faker::Games::Shadowrun
+
+```ruby
+Faker::Games::Shadowrun.metatype #=> Nocturne
+
+Faker::Games::Shadowrun.region #=> SOX
+
+Faker::Games::Shadowrun.city #=> Hong Kong Free Enterprise Zone
+
+Faker::Games::Shadowrun.street_name #=> Scarecrow
+
+Faker::Games::Shadowrun.critter #=> Barghest
+```
--- lib/faker/games/shadowrun.rb
+++ lib/faker/games/shadowrun.rb
@@ -8,20 +8,20 @@ module Faker
           fetch('games.shadowrun.metatype')
         end
 
-        def city
-          fetch('games.shadowrun.city')
-        end
-
-        def critter
-          fetch('games.shadowrun.critter')
-        end
-
         def region
           fetch('games.shadowrun.region')
         end
 
+        def city
+          fetch('games.shadowrun.city')
+        end
+
         def street_name
           fetch('games.shadowrun.street_name')
+        end
+
+        def critter
+          fetch('games.shadowrun.critter')
         end
       end
     end
